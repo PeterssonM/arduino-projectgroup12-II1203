dfff842b56670066d80620f07f8ed34e5094ec21
Maximilian Petersson <maxpet@kth.se>
2022-05-05 15:04:37 +0200

    first version of script

---
+++ ESP32ToFirebase/src/firebase_cred.h
@@ -0,0 +1,9 @@
+// Insert Firebase project API Key
+#define API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY"
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL "REPLACE_WITH_THE_AUTHORIZED_USER_EMAIL"
+#define USER_PASSWORD "REPLACE_WITH_THE_AUTHORIZED_USER_PASSWORD"
+
+// Insert Firebase storage bucket ID e.g bucket-name.appspot.com
+#define STORAGE_BUCKET_ID "REPLACE_WITH_YOUR_STORAGE_BUCKET_ID"
